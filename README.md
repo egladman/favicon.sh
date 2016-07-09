db7635d0362e96991f062cddeccb191991db89bf
Eli Gladman <eli.gladman@gmail.com>
2016-07-08 21:08:22 -0400

    First Commit :stuck_out_tongue:

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # favicon.sh
-A script that generates favicons
+A script that generates favicons of *all* variants
+
+#### Usage
+```bash
+./favicon.sh myImage.png
+```
+
+If favicon.sh is executed without an argument the script looks for `favicon.png`.
+If `favicon.png` doesn't exist the script exits.
+
+<br>
+
+Once the favicons are generated refer [here](https://github.com/audreyr/favicon-cheat-sheet)
---
+++ favicon.sh
@@ -0,0 +1,69 @@
+# A script that generates favicons of *all* variants
+# https://github.com/egladman/favicon.sh
+# Licensed under Apache License 2.0
+#
+# Created by Eli Gladman <eli.gladman@gmail.com>
+#
+# Usage:
+# ./favicon.sh path/to/myImage.png
+
+dependencies=(
+  "convert"
+)
+
+# check if dependencies are indeed installed
+for program in "${dependencies[@]}"
+do
+  command -v $program >/dev/null 2>&1 || {
+    echo >&2 "${program} is not installed. Aborting."
+    exit 1
+  }
+done
+
+
+len=$(echo -n $1 | wc -c)
+
+if [ $len == "0" ]; then
+  src="favicon.png"
+else
+  src=$1
+fi
+
+if [ ! -f $src ]; then
+  echo "${src} doesn't exist. Aborting"
+  exit 1
+fi
+
+
+ico=(
+  16 24 32 48 64
+)
+
+for i in "${ico[@]}"
+do
+  convert ${src} -resize ${i}x${i} favicon-${i}.png
+done
+
+
+png=(
+  32 57 76 96 120 128 144 152 180 195 196 228 270 558
+)
+
+for i in "${png[@]}"
+do
+  case ${i} in
+    128)
+      convert ${src} -resize ${i}x${i} smalltile.png
+      convert ${src} -resize ${i}x${i} favicon-${i}.png
+      ;;
+    270)
+      convert ${src} -resize ${i}x${i} mediumtile.png
+      ;;
+    558)
+      convert ${src} -resize ${i}x270 favicon-${i}.png
+      ;;
+    *)
+      convert ${src} -resize ${i}x${i} favicon-${i}.png
+      ;;
+  esac
+done
