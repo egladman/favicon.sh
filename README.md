5ba7d6ec89b582513e655affb77ae688c9166d10
Eli Gladman <eli.gladman@gmail.com>
2016-07-13 06:46:02 -0400

    fixed windows tiles and safari pinned tab

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # [favicon.sh](https://github.com/egladman/favicon.sh)
 A bash script that generates and compresses favicons of *all* variants. I'm by
-no means a bash expert, so if you see an error in my ways kindly let me know. I
-have no intention of making this POSIX compliant.
+no means a bash expert, so if you see an error in my ways kindly let me know.
 
-<!-- <br>
+<br>
 
 #### Support
-- iOS 6+ -->
-
+- iOS 6+
+- Safari Pinned Tab
+- Windows Tiles
 
 <br>
 
@@ -18,9 +18,15 @@ have no intention of making this POSIX compliant.
 <br>
 
 #### Usage
-```bash
-./favicon.sh <image> <output_path>
 ```
+./favicon.sh  -i path/to/image -o path/to/directory -c #bb0000
+```
+
+There are *4* options
+- `-h`  show the help text
+- `-i`  path to image (default: favicon.svg)
+- `-o`  set output directory (default: favicons)
+- `-c`  set background hex color of Windows tiles and Safari pinned tab (default: #0078d7)
 
 <br>
 
@@ -31,20 +37,9 @@ have no intention of making this POSIX compliant.
 
 <br>
 
-#### Default Behavior
-- If executed without any arguments the script looks for `favicon.png`. If
-`favicon.png` doesn't exist the script exits.
-
-- Generated favicons are saved to `/favicons` if a second argument isn't provided
-
-- Currently you cannot specify a output directory without first specifying an
-image path
-
-<br>
-
 #### Recommendations
 - The source image should be greater than or equal to `558x558`
-- Use a `.svg` for the source image
+- Use a `.svg` for the original image
 
 <br>
 
@@ -52,35 +47,34 @@ image path
 Insert the following lines into the head of your file
 
 ```html
-<link rel="icon" type="image/x-icon" href="/path/to/favicon.ico" sizes="16x16 24x24 32x32 48x48 64x64"/>
+<link rel="icon" type="image/x-icon" href="/path/to/favicon.ico" sizes="16x16 24x24 32x32 48x48 64x64">
+<link rel="mask-icon" href="/path/to/safari-pinned-tab.svg" color="#4285F4">
 
-<link rel='mask-icon' href='/path/to/icon.svg' color='#FFFFFF'>
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-57.png" sizes="57x57">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-60.png" sizes="60x60">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-72.png" sizes="72x72">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-76.png" sizes="76x76">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-114.png" sizes="114x114">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-120.png" sizes="120x120">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-144.png" sizes="144x144">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-152.png" sizes="152x152">
+<link rel="apple-touch-icon-precomposed" href="/path/to/favicons/favicon-180.png" sizes="180x180">
 
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-57.png" sizes="57x57">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-60.png" sizes="60x60">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-72.png" sizes="72x72">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-76.png" sizes="76x76">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-114.png" sizes="114x114">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-120.png" sizes="120x120">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-144.png" sizes="144x144">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-152.png" sizes="152x152">
-<link rel="apple-touch-icon-precomposed" href="/path/to/favicon-180.png" sizes="180x180">
-
-<meta name="msapplication-TileColor" content="#FFFFFF">
-<meta name="msapplication-TileImage" content="/path/to/favicon-144.png">
+<meta name="msapplication-TileColor" content="#4285F4">
+<meta name="msapplication-TileImage" content="/path/to/favicons/favicon-144.png">
 
 <meta name="application-name" content="Foo">
-<meta name="msapplication-tooltip" content="Tooltip">
-<meta name="msapplication-config" content="/path/to/ieconfig.xml">
+<meta name="msapplication-tooltip" content="Bar">
+<meta name="msapplication-config" content="/path/to/favicons/ieconfig.xml">
 
-<link rel="icon" type="image/png" href="/path/to/favicon-32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/path/to/favicon-96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/path/to/favicon-128.png" sizes="128x128">
-
-<link rel="icon" type="image/png" href="/path/to/favicon-195.png" sizes="195x195">
-<link rel="icon" type="image/png" href="/path/to/favicon-196.png" sizes="196x196">
-<link rel="icon" type="image/png" href="/path/to/favicon-228.png" sizes="228x228">
+<link rel="icon" type="image/png" href="/path/to/favicons/favicon-32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/path/to/favicons/favicon-96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/path/to/favicons/favicon-128.png" sizes="128x128">
+<link rel="icon" type="image/png" href="/path/to/favicons/favicon-195.png" sizes="195x195">
+<link rel="icon" type="image/png" href="/path/to/favicons/favicon-196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="/path/to/favicons/favicon-228.png" sizes="228x228">
 ```
+*Make sure to change out* `Foo` *and* `Bar`.
 
 <br>
 
@@ -88,3 +82,9 @@ Insert the following lines into the head of your file
 - [Favicon Cheat Sheet](https://github.com/audreyr/favicon-cheat-sheet)
 - [Apple Developer Docs](https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/pinnedTabs/pinnedTabs.html)
 - [Taylor Fausak](http://taylor.fausak.me/2015/01/27/ios-8-web-apps/)
+- [Microsoft Developer Network](https://msdn.microsoft.com/en-us/windows/uwp/controls-and-patterns/tiles-and-notifications-app-assets)
+
+<br>
+
+#### Contributing
+Contributions and/or feature requests are welcomed. Please report issues.
--- favicon.sh
+++ favicon.sh
@@ -6,33 +6,34 @@
 # Created by Eli Gladman <eli.gladman@gmail.com>
 #
 # Usage:
-# ./favicon.sh path/to/myImage.png
+# ./favicon.sh -i image.png -o path/to/directory
 
-red='\033[0;31m'
-orange='\033[0;33m'
-light_green='\033[0;32m'
-nc='\033[0m' # No Color
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+orange=$(tput setaf 3)
+purple=$(tput smul)
+normal=$(tput sgr0 rmul)
 
 success () {
-  echo -e "${light_green}Success: ${nc}$1"
+  printf '%s\n' "${green}Success: $normal$1" >&2
 }
 
 warning () {
-  echo -e "${orange}Warning: ${nc}$1"
+  printf '%s\n' "${orange}Warning: $normal$1" >&2
 }
 
 error () {
-  echo -e "${red}Error: ${nc}$1 Aborting."
-  exit 1
+  printf '%s\n' "${red}Error: $normal$1 Aborting." >&2
 }
 
 length () {
-  echo -n $1 | wc -c
+  printf $1 | wc -c
 }
 
 dependencies=(
   "convert"
   "optipng"
+  "getopts"
 )
 
 # check if dependencies are installed
@@ -43,18 +44,57 @@ do
   }
 done
 
-# set default image source if argument wasn't passed in
-if [ $(length $1) -eq "0" ]; then
-  source_image="favicon.png"
-else
-  source_image=$1
-fi
+usage="${0##*/} [-h] [-i path/to/image] [-o path/to/directory] [-c #bb0000] [-t 30] -- program to generate favicons
+where:
+  -h  show this help text
+  -i  path to image (default: favicon.svg)
+  -o  set output directory (default: favicons)
+  -c  set background hex color of Windows tiles and Safari pinned tab (default: #0078d7)
+  -t  monochromatic threshold for Safari pinned tab on a scale from 0 to 100 (default: 15)"
+
+path="favicons"
+source_image="favicon.svg"
+color="#0078d7"
+threshold="15"
+
+while getopts ':hc:i:t:o:' option; do
+  case $option in
+    h)
+      printf '%s\n' "$usage" >&2
+      exit
+      ;;
+    i)
+      source_image=$OPTARG
+      ;;
+    o)
+      path=$OPTARG
+      ;;
+    c)
+      color=$OPTARG
+      ;;
+    t)
+      threshold=$OPTARG
+      ;;
+    :)
+      error "missing argument for -$OPTARG."
+      printf '%s\n' "$usage" >&2
+      exit 1
+      ;;
+   \?)
+      error "illegal option -$OPTARG."
+      printf '%s\n' "$usage" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
 
 # if the file exists
 if [ -f $source_image ]; then
   dimensions=( $(identify -format '%W %H' $source_image) )
 else
   error "$source_image doesn't exist."
+  exit 1
 fi
 
 # difference in source images width and height
@@ -64,19 +104,19 @@ if [ $difference -gt 0 ]; then
   warning "$source_image is not a square image. Favicons will be distorted."
 fi
 
-ico_resolutions=(
+ico_sizes=(
   16 24 32 48 64
 )
 
-png_resolutions=(
+png_sizes=(
   16 24 32 48 57 60 64 72 76 96 114 120 128 144 152 180 195 196 228 270 558
 )
 
 for i in "${dimensions[@]}"
 do
-  if [ $i -lt ${png_resolutions[-1]} ] && [[ ! $source_image =~ \.svg$ ]]; then
-    min_resolution=${png_resolutions[-1]}x${png_resolutions[-1]}
-    warning "The image's resolution is less than the recommended $min_resolution."
+  if [ $i -lt ${png_sizes[-1]} ] && [[ ! $source_image =~ \.svg$ ]]; then
+    min_size=${png_sizes[-1]}x${png_sizes[-1]}
+    warning "The image's size is less than the recommended $min_size resolution."
 
     while :
     do
@@ -95,38 +135,32 @@ do
           exit 1
           ;;
         *)
-          echo "Please enter \"y\" or \"n\""
+          printf '%s\n' "Please enter \"y\" or \"n\""
           ;;
       esac
     done
   fi
 done
 
-# set default output path if second argument wasn't passed in
-if [ $(length $2) -eq "0" ]; then
-  path="favicons"
-else
-  path=$2
-fi
-
 # create directory if it doesn't exist
 if [ ! -d $path ]; then
   mkdir $path
 fi
 
-for i in "${png_resolutions[@]}"
+for i in "${png_sizes[@]}"
 do
+  # NOTE: This could use some refactoring. It's rather redundant
   case $i in
     128)
-      convert $source_image -resize ${i}x${i} $path/smalltile.png
+      convert $source_image -background none -resize $(expr $i / 2)x$(expr $i / 2) -gravity center -extent ${i}x${i} $path/smalltile.png
       convert $source_image -resize ${i}x${i} $path/favicon-$i.png
       ;;
     270)
-      convert $source_image -resize ${i}x${i} $path/mediumtile.png
+      convert $source_image -background none -resize $(expr $i / 3)x$(expr $i / 3) -gravity center -extent ${i}x${i} $path/mediumtile.png
       ;;
     558)
-      convert $source_image -resize ${i}x${i} $path/largetile.png
-      convert $source_image -resize ${i}x270 $path/widetile.png
+      convert $source_image -background none -resize $(expr $i / 3)x$(expr $i / 3) -gravity center -extent ${i}x${i} $path/largetile.png
+      convert $source_image -background none -resize $(expr $i / 6)x$(expr $i / 6) -gravity center -extent ${i}x270 $path/widetile.png
       ;;
     *)
       convert $source_image -resize ${i}x${i} $path/favicon-$i.png
@@ -134,20 +168,30 @@ do
   esac
 done
 
-# SVG vector mask for Safari 9+
-convert $source_image -resize 16x16 -colorspace gray $path/icon.svg
+# svg vector mask for Safari 9+
+convert $source_image -monochrome -threshold $threshold% -transparent white -resize 16x16 $path/safari-pinned-tab.svg
 
 # compress images
-optipng -o7 -strip all $path/*.png >/dev/null 2>&1
+if optipng -o7 -strip all $path/*.png >/dev/null 2>&1; then
+  success "Images compressed."
+else
+  error "A problem occured during compression."
+  exit 1
+fi
 
 files=()
-for i in "${ico_resolutions[@]}"
+for i in "${ico_sizes[@]}"
 do
   files+=($path/favicon-$i.png)
 done
 
 # consolidate multiple .png into .ico
-convert ${files[@]} $path/favicon.ico
+if convert ${files[@]} $path/favicon.ico; then
+  success "The favicons can be found in directory: $path"
+else
+  error "A problem occured while generating the .ico image."
+  exit 1
+fi
 
 # NOTE: some versions of "find" don't have the delete option. I might need to
 # refactor the following command to accommodate additional users
@@ -156,7 +200,7 @@ convert ${files[@]} $path/favicon.ico
 find $path -type f -name '*[a-z]-[0-2||4||6][0-9].png' -delete
 
 # generate ieconfig.xml as it's needed for Windows
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+printf "<?xml version=\"1.0\" encoding=\"utf-8\"?>
   <browserconfig>
     <msapplication>
       <tile>
@@ -164,9 +208,38 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
         <square150x150logo src=\"/path/to/mediumtile.png\"/>
         <wide310x150logo src=\"/path/to/widetile.png\"/>
         <square310x310logo src=\"/path/to/largetile.png\"/>
-        <TileColor>#FFFFFF</TileColor>
+        <TileColor>$color</TileColor>
       </tile>
     </msapplication>
   </browserconfig>" > $path/ieconfig.xml
 
-success "The favicons can be found in $path"
+printf '\n%s\n' "${purple}Use the following HTML${normal}" >&2
+
+# generate html
+printf '%s\n' "
+<link rel=\"icon\" type=\"image/x-icon\" href=\"/path/to/favicon.ico\" sizes=\"16x16 24x24 32x32 48x48 64x64\">
+<link rel=\"mask-icon\" href=\"/path/to/safari-pinned-tab.svg\" color=\"$color\">
+
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-57.png\" sizes=\"57x57\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-60.png\" sizes=\"60x60\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-72.png\" sizes=\"72x72\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-76.png\" sizes=\"76x76\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-114.png\" sizes=\"114x114\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-120.png\" sizes=\"120x120\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-144.png\" sizes=\"144x144\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-152.png\" sizes=\"152x152\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"/path/to/$path/favicon-180.png\" sizes=\"180x180\">
+
+<meta name=\"msapplication-TileColor\" content=\"$color\">
+<meta name=\"msapplication-TileImage\" content=\"/path/to/$path/favicon-144.png\">
+
+<meta name=\"application-name\" content=\"Foo\">
+<meta name=\"msapplication-tooltip\" content=\"Bar\">
+<meta name=\"msapplication-config\" content=\"/path/to/$path/ieconfig.xml\">
+
+<link rel=\"icon\" type=\"image/png\" href=\"/path/to/$path/favicon-32.png\" sizes=\"32x32\">
+<link rel=\"icon\" type=\"image/png\" href=\"/path/to/$path/favicon-96.png\" sizes=\"96x96\">
+<link rel=\"icon\" type=\"image/png\" href=\"/path/to/$path/favicon-128.png\" sizes=\"128x128\">
+<link rel=\"icon\" type=\"image/png\" href=\"/path/to/$path/favicon-195.png\" sizes=\"195x195\">
+<link rel=\"icon\" type=\"image/png\" href=\"/path/to/$path/favicon-196.png\" sizes=\"196x196\">
+<link rel=\"icon\" type=\"image/png\" href=\"/path/to/$path/favicon-228.png\" sizes=\"228x228\">" >&2
